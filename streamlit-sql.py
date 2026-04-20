be0a505836fb57f02ab00f8872dfa7fe4db66340
SanteIsHere <r.a.frye00@gmail.com>
2026-04-20 17:31:42 -0400

    Updated streamlit apps

--- streamlit-sql.py
+++ streamlit-sql.py
@@ -10,20 +10,19 @@ import os
 
 load_dotenv()
 
+
 def connect_to_db():
     # SQL_PASS must be defined in .env.
-    pw = os.environ.get('SQL_PASS')
+    pw = os.environ.get("SQL_PASS")
 
     # Connect to DB
     mydb = mysql.connector.connect(
-      host="localhost",
-      user="root",
-      password=pw,
-      database="cardb"
+        host="localhost", user="root", password=pw, database="cardb"
     )
 
     # Return database connection and cursor object for executing queries
     return mydb, mydb.cursor()
+
 
 def close_connection(db, cursor):
     cursor.close()
@@ -31,50 +30,50 @@ def close_connection(db, cursor):
 
 
 def get_user_selections():
-    with st.form(key='my_form'):
-        retail_range = st.slider('Retail price ($)',
-                        0, 100000, (20000, 50000))
-        min_mpg = st.slider('Minimum fuel efficiency (highway - mpg)',
-                        0, 55, 20)
-        car_type = st.radio(
-            'Car type',
-            ('Sedan', 'Wagon', 'SUV', 'Sports Car')
-        )
-        submitted = st.form_submit_button(label='Process')
+    with st.form(key="my_form"):
+        retail_range = st.slider("Retail price ($)", 0, 100000, (20000, 50000))
+        min_mpg = st.slider("Minimum fuel efficiency (highway - mpg)", 0, 55, 20)
+        car_type = st.radio("Car type", ("Sedan", "Wagon", "SUV", "Sports Car"))
+        submitted = st.form_submit_button(label="Process")
 
         if submitted:
             return retail_range, min_mpg, car_type
         else:
-            return ((20000, 50000), 20, 'Sedan')
+            return ((20000, 50000), 20, "Sedan")
 
 
 def exec_query(retail_range, min_mpg, car_type, cursor):
     # SQL query is broken down into multiple f strings for readibility.
     # f strings are concatenated automatically because of parenthesization.
     min_p, max_p = retail_range
-    query = (f"SELECT Name, `Retail Price`, `Highway Miles Per Gallon`, Type"
-             f" FROM cars WHERE `Retail Price` BETWEEN {min_p} AND {max_p}"
-             f" AND `Highway Miles Per Gallon` > {min_mpg}"
-             f" AND Type = '{car_type}';")
+    query = (
+        f"SELECT Name, `Retail Price`, `Highway Miles Per Gallon`, Type"
+        f" FROM cars WHERE `Retail Price` BETWEEN {min_p} AND {max_p}"
+        f" AND `Highway Miles Per Gallon` > {min_mpg}"
+        f" AND Type = '{car_type}';"
+    )
     print(query)
     # Execute the SQL query
     cursor.execute(query)
 
     # Put results in a DataFrame
     columns = [desc[0] for desc in cursor.description]
-    results_df = pd.DataFrame(cursor.fetchall(), columns = columns)
+    results_df = pd.DataFrame(cursor.fetchall(), columns=columns)
     return results_df
 
+
 def main():
-    st.title('Car database')
-    st.subheader('MySQL-based implementation')
+    st.title("Car database")
+    st.subheader("MySQL-based implementation")
+    st.subheader("Activity 9: Asante Frye")
     db, cursor = connect_to_db()
     retail_range, min_mpg, car_type = get_user_selections()
     results = exec_query(retail_range, min_mpg, car_type, cursor)
-    st.markdown('---')
-    st.subheader('Matches')
-    st.dataframe(results, width = 900, height = 300)
+    st.markdown("---")
+    st.subheader("Matches")
+    st.dataframe(results, width=900, height=300)
     close_connection(db, cursor)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
